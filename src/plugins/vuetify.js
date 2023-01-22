74c71b9e4d75e922779ae1f8d150eb9ca1f2c587
NeneAminatou <neneaminatou.diallo@gmail.com>
2023-01-22 13:00:24 +0100

    deploy live

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -2,6 +2,16 @@ import Vue from 'vue';
 import Vuetify from 'vuetify/lib/framework';
 
 Vue.use(Vuetify);
+const vuetify = new Vuetify({
+  theme: {
+    themes: {
+      light: {
+        primary: '#FFFFFF', //white
+        secondary: '#4CAF50', //green
 
-export default new Vuetify({
-});
+      },
+    },
+  },
+})
+
+export default vuetify
