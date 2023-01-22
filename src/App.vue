0d70e0af9d4a22d3dc81b4be8018771abadf7ccb
NeneAminatou <neneaminatou.diallo@gmail.com>
2023-01-22 13:57:45 +0100

    test pied de page

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <!-- eslint-disable vue/max-attributes-per-line -->
 <template>
   <v-app>
+    <!-- Entete de l'application -->
     <v-app-bar app color="secondary" dark>
       <v-app-bar-nav-icon></v-app-bar-nav-icon>
 
@@ -14,16 +15,26 @@
       <v-spacer> </v-spacer>
       <v-icon>mdi-bell</v-icon>
     </v-app-bar>
+    <!-- Fin de l'entete de l'application-->
 
+    <!-- corps de la pagede l'application-->
     <v-main>
       <router-view />
     </v-main>
+    <!-- fin du contenu du corps -->
+
+
+    <!-- debut pied de page -->
+    <mybottom-navigation></mybottom-navigation>
   </v-app>
+
 </template>
 
 <script>
+import MybottomNavigation from './components/MybottomNavigation.vue';
 export default {
-  name: "App",
+  components: { MybottomNavigation },
+
 
   data: () => ({
     //
---
+++ src/components/MybottomNavigation.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+
+    <v-bottom-navigation app background-color="primary" color="secondary" grow>
+      <v-btn value="plantes">
+        <span>Plantes</span>
+
+        <v-icon>mdi-flower</v-icon>
+      </v-btn>
+
+      <v-btn value="localisation">
+        <span>Localisation</span>
+
+        <v-icon>mdi-crosshairs-gps</v-icon>
+      </v-btn>
+
+      <v-btn value="forum">
+        <span>Forum</span>
+
+        <v-icon>mdi-forum</v-icon>
+      </v-btn>
+    </v-bottom-navigation>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
